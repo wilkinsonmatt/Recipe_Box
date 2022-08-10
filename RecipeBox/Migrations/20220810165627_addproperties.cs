3a34e3154bf037af71ea0a2ab447bf9926b2b867
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-10 14:40:15 -0700

    Added ingredients, rating, and instructions
    Co-authored-by: Claire Thorington <claire.thorington@gmail.com>

---
+++ RecipeBox/Migrations/20220810165627_addproperties.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBox.Migrations
+{
+    public partial class addproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ingredients",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instructions",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ingredients",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Instructions",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Recipes");
+        }
+    }
+}
